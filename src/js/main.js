64f17dda3469bb4ba5dbdc65637e00887674f6b9
alexrodriii <159908289+alexrodriii@users.noreply.github.com>
2024-04-30 11:43:34 +0200

    validator y slick

--- src/js/main.js
+++ src/js/main.js
@@ -87,3 +87,187 @@ function openPopup(content) {
     .catch(error => console.error('Error loading popup content:', error));
 }
 
+//JQUERY VALIDATE
+$("#login").validate ({
+
+  rules: {
+      username: {
+          required:true,
+          minlength: 3
+
+      },
+      password:  {
+          required:true,
+          minlength: 8
+
+      }
+  },
+
+  messages: {
+      username:  {
+          required: "El usuario es obligatorio ",
+          minlength: "El usuario debe tener minimo 3 caracteres"
+
+      },
+      password: {
+          required: "La contraseña es obligatoria",
+          minlength: "La contraseña debe tener minimo 8 caracteres"
+      }
+  }
+});
+
+$("#register").validate ({
+
+  rules: {
+      mail: {
+          required:true,   
+          email:true
+      },
+      username:{
+          required:true,
+          minlength:3,
+          maxlength:13
+      },
+      password: {
+          required: true,
+          minlength: 8
+      }
+  },
+  messages: {
+      mail: {
+          required: "El correo es obligatorio",
+          email: "Introduce una dirección válida"
+      },
+      username:{
+          required:"El usuario es obligatorio",
+          minlength:"Debe tener almenos 3 caracteres",
+          maxlength: "Debe ser de macimo 13 caracteres"
+      },
+      password: {
+          required: "La contraseña es obligatoria",
+          minlength:"La contraseña debe tener minimo 8 caracteres"
+      }
+  }
+});
+
+$("#register-admin").validate ({
+
+  rules: {
+      mail: {
+          required:true,   
+          email:true
+      },
+      username:{
+          required:true,
+          minlength:3,
+          maxlength:13
+      },
+      password: {
+          required: true,
+          minlength: 8
+      }
+  },
+  messages: {
+      mail: {
+          required: "El correo es obligatorio",
+          email: "Introduce una dirección válida"
+      },
+      username:{
+          required:"El usuario es obligatorio",
+          minlength:"Debe tener almenos 3 caracteres",
+          maxlength: "Debe ser de máximo 13 caracteres"
+      },
+      password: {
+          required: "La contraseña es obligatoria",
+          minlength:"La contraseña debe tener minimo 8 caracteres"
+      }
+  }
+});
+
+$("#information").validate ({
+
+  rules: {
+      username: {
+          required: true,
+          maxlength: 13,
+          minlength:3
+      },
+      nameSurname: {
+          required: true,
+          maxlength: 25,
+          minlength: 3
+
+      },
+      email: {
+          required:true,   
+          email:true
+      },
+      birthDate: {
+          required:true,   
+              
+      },
+      phoneNumberr: {
+          required:true,   
+          minlength: 9    
+      }
+  
+  },
+  messages: {
+      username: {
+          required:"El usuario es obligatorio",
+          maxlength: "Debe ser de máximo 13 caracteres",
+          minlength:"Debe tener almenos 3 caracteres"
+      },
+      nameSurname: {
+          required: "El nombre y tu primer apellido son obligatorios",
+          maxlength: "Debe ser de máximo 25 caracteres",
+          minlength:"Debe tener almenos 3 caracteres"
+
+      },
+      email: {
+          required:"El correo es obligatorio",   
+          email:"Introduce una dirección válida"
+      },
+      birthDate: {
+          required:"La fecha de nacimiento es obligatoria",   
+          
+      },
+      phoneNumberr: {
+          required:"El telefono es obligatorio",   
+          minlength:"El número de telefono debe tener minimo 9 digitos"
+      }
+  }
+});
+
+// SLICK CARROUSEL
+$("#slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  prevArrow: "<button class='prev'>PREV</button>",
+  nextArrow: "<button class='next'>NEXT</button>",
+  responsive:[
+      {
+          breakpoint: 800,
+          settings:{
+              slidesToShow: 2,
+              arrows: false,
+              dots: false
+          }
+      },
+
+      {
+          breakpoint: 600,
+          settings:{
+              slidesToShow: 1,
+              arrows: false,
+              dots: false,
+          }
+      }
+  ]
+});
