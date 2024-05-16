310ecec3d54e17d0c12d2a4ea6d989edfefd645c
Pau Carrera <poksy2002@gmail.com>
2024-05-16 18:25:51 +0200

    style(css): improve wishlist page design

--- src/view/coupons-popup.php
+++ src/view/coupons-popup.php
@@ -5,16 +5,16 @@
         <div class="info-form aligned">
             <h2>Discount Coupons</h2>
             <article>
-                <div class="coupon">
+                <div class="coupon div-popup">
                     <div class="coupon-details">
                         <h3>Honkaku II - 3x2</h3>
                         <p>This coupon allows you to get a 3-for-2 deal at Honkaku II restaurant.</p>
                     </div>
-                    <div class="coupon-action">
+                    <div class="coupon-action ">
                         <button>Show Coupon</button>
                     </div>
                 </div>
-                <div class="coupon">
+                <div class="coupon div-popup">
                     <div class="coupon-details">
                         <h3>KFC - Menu Savings</h3>
                         <p>Enjoy a special discount on selected menu items at KFC.</p>
--- src/view/wishlist-popup.php
+++ src/view/wishlist-popup.php
@@ -6,18 +6,57 @@
             <h2>Wishlist</h2>
             <article>
                 <div class="div-popup">
-                    <p>
-                        1. McDonald's: Big Mac Combo for €4.50<br>
-                        2. Burger King: 2 Whoppers for €8.50<br>
-                        3. Subway: Sub of the Day for €4<br>
-                        4. KFC: Fried Chicken Bucket for €8.50<br>
-                        5. Domino's Pizza: 2 medium pizzas for €13<br>
-                        6. Taco Bell: Taco Combo for €4.50<br>
-                        7. Wendy's: Cheeseburger for €0.90
-                    </p>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>McDonald's</span>
+                            <small>- Big Mac Combo for €4.50</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>Burger King</span>
+                            <small>- 2 Whoppers for €8.50</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>Subway</span>
+                            <small>- Sub of the Day for €4</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>KFC</span>
+                            <small>- Fried Chicken Bucket for €8.50</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>Domino's Pizza</span>
+                            <small>- 2 medium pizzas for €13</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>Taco Bell</span>
+                            <small>- Taco Combo for €4.50</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
+                    <div class="wishlist-item">
+                        <div>
+                            <span>Wendy's</span>
+                            <small>- Cheeseburger for €0.90</small>
+                        </div>
+                        <i class="fas fa-bookmark"></i>
+                    </div>
                 </div>
             </article>
         </div>
     </div>
-
 </body>
