d12b68a25a1c0c85876a763d4e7ca4a109e3c578
Pau Carrera <poksy2002@gmail.com>
2024-05-29 16:28:45 +0200

    style(affiliation list): update page styles

--- src/view/list-affiliations.php
+++ src/view/list-affiliations.php
@@ -1,49 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabla con Imágenes y Botones</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <meta name="description" content="Flash Food gives you the best offers and discounts near you." />
+    <meta name="generator" content="FlashFood">
+    <title>FlashFood | Affiliations list</title>
+    <link rel="icon" type="image/png" href="../images/flashfoods-logo-f.png">
+
+    <!-- css -->
+    <link rel="stylesheet" href="../css/normal-page.css">
+
+    <!-- jQuery -->
+    <script src="/src/assets/js/jquery-3.7.1.min.js"></script>
+    <script src="/src/assets/js/slick-1.8.1/slick/slick.js"></script>
+    <script src="/src/assets/js/dist/jquery.validate.js"></script>
+    <script src="/src/assets/js/dist/additional-methods.js"></script>
 </head>
-<body>
+
+<body id="grey-background" class="center">
+    <div id="white-background">
+        <div class="div-logo">
+            <a href="../index.php"><img class="logo center" src="../images/flashfoods-logo.png"
+                    alt="logo-flashfood"></a>
+        </div>
+
+        <div id="div-title">
+            <a href="#">
+                <button id="technical-title">Affiliation list</button>
+            </a>
+        </div>
+    </div>
     <table>
         <thead>
             <tr>
-                <th class="img-col">Imagen</th>
-                <th class="text-col">Nombre</th>
-                <th class="text-col">Teléfono</th>
-                <th class="text-col">Correo</th>
-                <th class="text-col">Descripción</th>
-                <th class="button-col">Acciones</th>
+                <th class="img-col">Image</th>
+                <th class="text-col">Name</th>
+                <th class="text-col">Phone</th>
+                <th class="text-col">Email</th>
+                <th class="text-col">Description</th>
+                <th class="button-col">Actions</th>
             </tr>
         </thead>
         <tbody>
-        <?php
-        require_once '../controller/AdminController.php';
+            <?php
+            require_once '../controller/AdminController.php';
 
-        $admin = new AdminController();
-        $result = $admin->getAffiliations();
-        
-        if ($result->rowCount() > 0) {
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                echo "<tr>";
-                echo "<td class='img-col'><img src='../model/" . $row["image"] . "' alt='" . $row["name"] . "'></td>";
-                echo "<td class='text-col'>" . $row["name"] . "</td>";
-                echo "<td class='text-col'>" . $row["phone_number"] . "</td>";
-                echo "<td class='text-col'>" . $row["mail"] . "</td>";
-                echo "<td class='text-col'>" . $row["description"] . "</td>";
-                echo "<td class='button-col'>";
-                echo "<button type='submit' name='edit_affiliation' class='btn' onclick='openPopup(\"change-affiliation-popup.php\", \"" . $row["id_affiliation"] . "\")'>Actualizar</button>";
+            $admin = new AdminController();
+            $result = $admin->getAffiliations();
 
-                echo "<button class='btn'>Eliminar</button>";
-                echo "</td>";
-                echo "</tr>";
-            }                               
-        } else {
-            echo "<tr><td colspan='6'>No hay datos disponibles</td></tr>";
-        }
-        ?>
+            if ($result->rowCount() > 0) {
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<tr>";
+                    echo "<td class='img-col'><img src='../model/" . $row["image"] . "' alt='" . $row["name"] . "'></td>";
+                    echo "<td class='text-col'>" . $row["name"] . "</td>";
+                    echo "<td class='text-col'>" . $row["phone_number"] . "</td>";
+                    echo "<td class='text-col'>" . $row["mail"] . "</td>";
+                    echo "<td class='text-col'>" . $row["description"] . "</td>";
+                    echo "<td class='button-col'>";
+                    echo "<button type='submit' name='edit_affiliation' class='btn' onclick='openPopup(\"change-affiliation-popup.php\", \"" . $row["id_affiliation"] . "\")'>Update</button>";
+
+                    echo "<button class='btn'>Delete</button>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='6'>No data available</td></tr>";
+            }
+            ?>
 
         </tbody>
     </table>
@@ -57,4 +82,5 @@
     <!-- js -->
     <script src="../js/popups.js"></script>
 </body>
+
 </html>
